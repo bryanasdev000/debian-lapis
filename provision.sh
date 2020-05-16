b8549f601528c9e2e1841bc7ef53c328ca9e0725
Hector Vido <hector_vido@yahoo.com.br>
2020-05-16 01:19:04 -0300

    Adição da biblioteca correta SSL

--- provision.sh
+++ provision.sh
@@ -8,3 +8,15 @@ apt-get update
 apt-get -y install vim git lua51 luarocks openresty libssl-dev
 
 luarocks install lapis
+
+#  OpenSSL 1.1.1
+apt-get install -y build-essential
+cd /usr/src
+git clone -b OpenSSL_1_1_1-stable https://github.com/openssl/openssl.git
+cd openssl
+./config
+make -j 6
+make install
+rm /usr/local/openresty/openssl/lib/libcrypto.so.1.1 /usr/local/openresty/openssl/lib/libssl.so.1.1
+cp /usr/src/openssl/libcrypto.so.1.1 /usr/local/openresty/openssl/lib/libcrypto.so.1.1
+cp /usr/src/openssl/libssl.so.1.1 /usr/local/openresty/openssl/lib/libssl.so.1.1
