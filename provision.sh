1360783842ac68139e3352f81abd90a149ee387b
Bryan A. S <bryanasdev000@gmail.com>
2020-05-16 02:49:36 -0300

    Fix typos

--- provision.sh
+++ provision.sh
@@ -1,10 +1,10 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 apt-get -y install --no-install-recommends wget gnupg ca-certificates
 wget -O - https://openresty.org/package/pubkey.gpg | apt-key add -
 echo "deb http://openresty.org/package/debian buster openresty" > /etc/apt/sources.list.d/openresty.list
 apt-get update
-apt-get -y install vim git lua51 luarocks openresty libssl-dev
+apt-get -y install vim git lua5.1 luarocks openresty libssl-dev
 
 luarocks install lapis
 
